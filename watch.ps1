fdd4d35d41a155d1848ba2a369f7013d4dc26323
Johann Goltz <johann-goltz@hotmail.de>
2020-06-18 10:32:37 +0200

    pdftk has been installed -> remove "./"

--- watch.ps1
+++ watch.ps1
@@ -27,7 +27,7 @@ inotifywait -q -m -e CLOSE_WRITE $IN_DIR | Foreach-Object {
 
             Write-Information "Executing pdftk A=$previous_file_name B=$file_name shuffle A Bend-1 output $out_file"
             # todo does not ask before overwriting
-            ./pdftk A=$previous_file_name B=$file_name shuffle A Bend-1 output $out_file
+            pdftk A=$previous_file_name B=$file_name shuffle A Bend-1 output $out_file
 
             if (-not $?) {
                 throw "pdftk errored; exiting."
